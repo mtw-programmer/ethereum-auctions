f784cbabd24fa1977664ff405a494c0fee8546bf
mtw-programmer <visnia58@gmail.com>
2023-10-30 22:56:57 +0100

    feat: add createAuction function

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 
 contract Auctions {
   uint public auctionCount = 0;
+  address owner;
 
   struct Auction {
     uint id;
@@ -10,5 +11,22 @@ contract Auctions {
     uint256 startPrice;
     uint256 currentPrice;
     bool end;
+  }
+
+  event AuctionCreated (
+    uint id,
+    string title,
+    string description,
+    uint256 startPrice,
+    uint256 currentPrice,
+    bool end
+  );
+
+  mapping(uint => Auction) public auctions;
+
+  function createAuction (string memory _title, string memory _description, uint256 memory _startPrice) public {
+    auctionCount++;
+    auctions[auctionCount] = Auction(auctionCount, _title, _description, _startPrice, _startPrice, false);
+    emit AuctionCreated(auctionCount, _title, _description, _startPrice, _startPrice, false);
   }
 }
