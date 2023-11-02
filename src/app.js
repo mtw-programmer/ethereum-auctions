0523d1fbf051a6be806af1618cad583dbcad37ca
mtw-programmer <visnia58@gmail.com>
2023-11-02 12:26:15 +0100

    fix: replace parseInt with parseFloat in app.js

--- src/app.js
+++ src/app.js
@@ -59,7 +59,7 @@ const createAuction = async () => {
     app.setLoading(true);
     const title = $('#title').val();
     const description = $('#description').val();
-    const price = Math.round(parseInt($('#price').val()) * 100);
+    const price = Math.round(parseFloat($('#price').val()) * 100);
 
     if (title && description && price) {
       await app.auctions.createAuction(title, description, price, { from: app.account });
@@ -124,17 +124,17 @@ const renderAuctions = async () => {
 
 const placeBid = async (id) => {
   try {
-    const newPrice = Math.round(parseInt($(`.enter-bid[name=${id}]`).val()) * 100);
+    const newPrice = Math.round(parseFloat($(`.enter-bid[name=${id}]`).val()) * 100);
     if (!newPrice || !id) return;
     app.setLoading(true);
 
     await app.auctions.PlaceBid(id, newPrice, { from: app.account });
     await delay(500);
-    window.location.reload();
   } catch (ex) {
     console.error(ex);
   } finally {
     app.setLoading(false);
+    window.location.reload();
   }
 };
 
