4ef4925add654a6cc4274e6db3374d910046331c
mtw-programmer <visnia58@gmail.com>
2023-11-06 12:19:07 +0100

    refactor: separate renderAuctions to a few functions

--- src/js/render.mjs
+++ src/js/render.mjs
@@ -23,74 +23,78 @@ const getAuction = async (i) => {
   const endPrice = (auction[5].toNumber() / 100).toFixed(2);
   const end = auction[6];
 
-  return { auction, id, owner, title, description, startPrice, endPrice, end };
+  return { i, auction, id, owner, title, description, startPrice, endPrice, end };
 };
+
+const renderActiveAuctions = async (properties) => {
+  const newActiveTemplate = app.activeTemplate.clone();
+
+    newActiveTemplate.find('.title').html(properties.title);
+    newActiveTemplate.find('.description').html(properties.description);
+
+    newActiveTemplate.find('.currentPrice').html(properties.endPrice);
+
+    if (properties.owner.toLowerCase() == app.account.toLowerCase()) {
+      newActiveTemplate.find('.enter-bid')
+        .prop('name', properties.id);
+
+      newActiveTemplate.find('.bid-btn')
+        .prop('name', properties.id);
+
+      newActiveTemplate.find('.stop-bid-btn')
+        .prop('name', properties.id)
+        .removeProp('disabled');
+    }
+    else {
+      newActiveTemplate.find('.enter-bid')
+        .prop('name', properties.id)
+        .prop('min', properties.endPrice)
+        .removeProp('disabled');
+
+      newActiveTemplate.find('.bid-btn')
+        .prop('name', properties.id)
+        .removeProp('disabled');
+    }
+
+    $('.active-auctions').append(newActiveTemplate);
+    newActiveTemplate.show();
+};
+
+const renderEndAuctions = async (properties) => {
+  const newStopTemplate = app.stopTemplate.clone();
+
+  newStopTemplate.find('.title').html(properties.title);
+  newStopTemplate.find('.description').html(properties.description);
+
+  const lastBid = await app.auctions.getLastBidIndex(properties.i);
+
+  if (lastBid.toNumber() - 1 < 0) {
+    newStopTemplate.find('.auctionWinner').html('-');
+    newStopTemplate.find('.startPrice').html(properties.startPrice);
+    newStopTemplate.find('.endPrice').html('-');
+  } else {
+    const bid = await app.auctions.bids(properties.i, lastBid.toNumber() - 1);
+
+    newStopTemplate.find('.auctionWinner').html(bid.bidder);
+    newStopTemplate.find('.startPrice').html(properties.startPrice);
+    newStopTemplate.find('.endPrice').html(properties.endPrice);
+  }
+
+  $('.stop-auctions').append(newStopTemplate);
+  newStopTemplate.show();
+}
 
 const renderAuctions = async () => {
   try {
     const auctionCount = await app.auctions.auctionCount();
 
     for (let i = 1; i <= auctionCount; i++) {
-      const { auction, id, owner,
-        title, description, startPrice,
-        endPrice, end } = await getAuction(i);
+      const properties = await getAuction(i);
 
-      if (!end) {
-        const newActiveTemplate = app.activeTemplate.clone();
-
-        newActiveTemplate.find('.title').html(title);
-        newActiveTemplate.find('.description').html(description);
-
-        const currentPrice = auction[5].toNumber();
-        newActiveTemplate.find('.currentPrice').html((currentPrice / 100).toFixed(2));
-
-        if (owner.toLowerCase() == app.account.toLowerCase()) {
-          newActiveTemplate.find('.enter-bid')
-            .prop('name', id);
-
-          newActiveTemplate.find('.bid-btn')
-            .prop('name', id);
-
-          newActiveTemplate.find('.stop-bid-btn')
-            .prop('name', id)
-            .removeProp('disabled');
-        }
-        else {
-          newActiveTemplate.find('.enter-bid')
-            .prop('name', id)
-            .prop('min', (currentPrice / 100 + 0.01).toFixed(2))
-            .removeProp('disabled');
-
-          newActiveTemplate.find('.bid-btn')
-            .prop('name', id)
-            .removeProp('disabled');
-        }
-
-        $('.active-auctions').append(newActiveTemplate);
-        newActiveTemplate.show();
-      } else {
-        const newStopTemplate = app.stopTemplate.clone();
-
-        newStopTemplate.find('.title').html(title);
-        newStopTemplate.find('.description').html(description);
-
-        const lastBid = await app.auctions.getLastBidIndex(i);
-
-        if (lastBid.toNumber() - 1 < 0) {
-          newStopTemplate.find('.auctionWinner').html('-');
-          newStopTemplate.find('.startPrice').html(startPrice);
-          newStopTemplate.find('.endPrice').html('-');
-        } else {
-          const bid = await app.auctions.bids(i, lastBid.toNumber() - 1);
-
-          newStopTemplate.find('.auctionWinner').html(bid.bidder);
-          newStopTemplate.find('.startPrice').html(startPrice);
-          newStopTemplate.find('.endPrice').html(endPrice);
-        }
-
-        $('.stop-auctions').append(newStopTemplate);
-        newStopTemplate.show();
-      }
+      if (!properties.end)
+        await renderActiveAuctions(properties);
+      else
+        await renderEndAuctions(properties);
     }
   } catch (ex) {
     console.error(ex);
